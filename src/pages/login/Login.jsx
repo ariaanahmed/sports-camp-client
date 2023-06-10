258d19c5421cacfba16c941f0e72bd3a11523c23
ariaan ahmed <you@example.com>
2023-06-10 13:10:53 +0600

    firebase added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
 
 const Login = () => {
+    const hanldeLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+    } 
     return (
         <div className="pt-5">
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
+                        <form onSubmit={hanldeLogin} className="card-body">
                             <h3 className="font-bold text-3xl">Please Login</h3>
                             <div className="form-control">
                                 <label className="label">
@@ -18,18 +26,18 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" required placeholder="Password" className="input input-bordered" />
+                                <input type="password" name="password" required placeholder="Password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <input type="submit" value="Login" className="btn btn-primary" />
                             </div>
                             <label className="label">
                                 <Link to="/signup" className="label-text-alt link link-hover">New to A12? Sign Up!</Link>
                             </label>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
