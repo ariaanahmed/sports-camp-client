9004a52b91b98aaa6dfcd20ed758f8abd99e90f1
ariaan ahmed <you@example.com>
2023-06-12 16:49:39 +0600

    my classes delete button implemented

--- src/pages/shared/dashboard/myClasses/MyClasses.jsx
+++ src/pages/shared/dashboard/myClasses/MyClasses.jsx
@@ -3,7 +3,7 @@ import useBooked from "../../../../hooks/useBooked";
 
 const MyClasses = () => {
     const [booked] = useBooked();
-    const total = booked.reduce( (sum, bookedClass) => bookedClass.price + sum, 0)
+    const total = booked.reduce((sum, bookedClass) => bookedClass.price + sum, 0)
     return (
         <div>
             <Helmet>
@@ -13,6 +13,45 @@ const MyClasses = () => {
                 <p className="bg-gray-50 p-2 rounded-md">Items: {booked.length}</p>
                 <p>Price: {total}</p>
                 <button className="btn btn-warning btn-sm">pay</button>
+            </div>
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Picture</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                booked.map((item, index) => <tr 
+                                    key={item._id}
+                                >
+                                <td>{index + 1}</td>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} alt="image" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='font-bold'>{item.name}</td>
+                                <td>$ {item.price}</td>
+                                <td>
+                                    <button className="btn btn-info btn-sm">Delete</button>
+                                </td>
+                            </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
