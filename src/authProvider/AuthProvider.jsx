b8098fc23966155d4116931a2fb34e7b5f33d226
ariaan ahmed <you@example.com>
2023-06-13 11:37:51 +0600

    axios and jwt added

--- src/authProvider/AuthProvider.jsx
+++ src/authProvider/AuthProvider.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { createContext } from "react";
-import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -45,6 +46,16 @@ const AuthProvider = ({children}) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
+
+            if (currentUser) {
+                axios.post('http://localhost:5000/jwt', { email: currentUser?.email })
+                    .then((data) => {
+                        localStorage.setItem('access-token', data.data.token)
+                    })
+            } else{
+                localStorage.removeItem('access-token')
+            }
+
             setLoading(false);
         })
         return () => {
